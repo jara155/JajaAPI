63c46339e102195291c1cd9867285c7761c14389
Jája <74739998+jara155@users.noreply.github.com>
2023-06-16 20:06:56 +0200

    Add files via upload

---
+++ JajaAPI/src/main/java/utils/Instances.java
@@ -0,0 +1,20 @@
+package utils;
+
+import handlers.Item;
+import handlers.Locales;
+
+public class Instances {
+    public static Strings stringsInstance = new Strings();
+    public static Strings updateChecker = new Strings();
+    public static Locales localesInstance = new Locales();
+    public static Item itemInstance = new Item();
+
+    public static void main(String[] args) {
+        Strings stringsInstance = new Strings();
+        Strings updateChecker = new Strings();
+        Locales localesInstance = new Locales();
+        Item itemInstance = new Item();
+    }
+
+
+}
